e0da6f9be00ab2a9ae77e9979c1477f2cb1a6b5d
William Garneau <33030290+Thyix@users.noreply.github.com>
2018-11-27 16:11:25 -0500

    Add files via upload

---
+++ TensorFlow/TensorFlow.py
@@ -0,0 +1,36 @@
+#########################################################
+#
+# Installing Python and OpenCV for python
+# Go to  C:\Users\usager\AppData\Local\Programs\Python\Python36\Scripts>
+# 
+# To install Python :  C:\Users\usager\AppData\Local\Programs\Python\Python36\Scripts> pip3 install --upgrade tensorflow
+# To install opencv :  C:\Users\usager\AppData\Local\Programs\Python\Python36\Scripts> pip3 install opencv-python
+#
+#########################################################
+
+import tensorflow as tf
+import numpy as np
+import sys
+import cv2
+import time
+
+import Helper as Hlp
+import NotesDeCours as NC
+import Exercice1 as e1
+import Exercice2 as e2
+#import TensorBoard as TB
+
+#########################################################
+#
+# Main body
+#
+#########################################################
+
+#e1.Classification()
+e2.TextClassification()
+#NC.EspaceReserve()
+#NC.Variable()
+#NC.Matrice()
+#NC.MultiNode();
+#NC.BaseGraphe(4,3)
+
