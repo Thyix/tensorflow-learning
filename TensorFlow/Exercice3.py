f3d39d5fc12ed605adc42e755a6f834e31f02d83
William Garneau <willi_garn@hotmail.com>
2018-11-30 10:31:55 -0500

    Created file for exercice 3

---
+++ TensorFlow/Exercice3.py
@@ -0,0 +1,18 @@
+##################################
+#
+# Tutorials #3 for Tensorflow
+#
+##################################
+
+# future lib import
+from __future__ import absolute_import, division, print_function
+
+# Tensorflow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
